df0958ff615a8956dd49fbd8e17976da7477e432
Will Mitchell <wvmitchell@gmail.com>
2020-01-06 15:25:38 -0700

    add function tests for pizza class

--- pizza-test.js
+++ pizza-test.js
@@ -29,4 +29,30 @@ describe('Pizza', function () {
 
     assert.deepEqual(pie1.toppings, []);
   })
+
+  it('should be able to add toppings', function () {
+    var pie1 = new Pizza('medium');
+
+    pie1.addTopping('Mushrooms');
+    pie1.addTopping('Sausage');
+
+    assert.deepEqual(pie1.toppings, ['Mushrooms', 'Sausage']);;
+  })
+
+  it('should tell me that I have successfully added a topping', function () {
+    var pie1 = new Pizza('medium');
+
+    assert.equal(pie1.addTopping('Mushrooms'), 'Great, you added Mushrooms')
+    assert.equal(pie1.addTopping('Sausage'), 'Great, you added Sausage')
+  })
+
+  it('should not be able to add more toppings than allowed', function () {
+    var pie1 = new Pizza('medium');
+
+    pie1.addTopping('Mushrooms');
+    pie1.addTopping('Sausage');
+
+    assert.equal(pie1.addTopping('Peppers'), 'Sorry, you have added all the toppings for this pizza');
+    assert.deepEqual(pie1.toppings, ['Mushrooms', 'Sausage']);;
+  })
 })
